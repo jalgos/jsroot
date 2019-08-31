b0e292a2cbb405ed1d0b4f6ad7db82a0971931ca
Ivan Diachenko <ivan@jalgos.com>
2019-08-31 17:32:42 +0200

    Split test on smaller tests to check different use cases. Make tests more isolated

--- tests/testthat/test.dependencies.R
+++ tests/testthat/test.dependencies.R
@@ -1,7 +1,15 @@
 context("dependencies")
 
+#' Removes jsroot_lib_test before each test to make sure each test is isolated
+clean_before <- function() {
+    system("rm -Rf jsroot_lib_test")
+    system("ls -la")
+}
+
 test_that("cran.packages dependencies work",
 {
+    clean_before()
+
     expect_output(jsroot::dependencies(cran.packages = c("track", "uuid"),
                                        libpath = 'jsroot_lib_test',
                                        force.cran = TRUE),
@@ -10,6 +18,8 @@ test_that("cran.packages dependencies work",
 
 test_that("cran.packages dependencies throw an error",
 {
+    clean_before()
+
     expect_warning(jsroot::dependencies(cran.packages = c("unknown_package"),
                                         libpath = 'jsroot_lib_test',
                                         force.cran = TRUE,
@@ -19,20 +29,25 @@ test_that("cran.packages dependencies throw an error",
 
 test_that("jspackages dependencies work",
 {
-    jsroot::dependencies(jspackages = list("utils" = c("jlogger")),
+    clean_before()
+
+    jsroot::dependencies(cran.packages = c("track", "uuid"), # jslogger track and uuid should be installed, beacase jlogger uses it
+                         jspackages = list("utils" = c("jlogger")),
                          libpath = 'jsroot_lib_test',
+                         force.cran = TRUE,
                          force.jalgos = TRUE,
                          quiet = FALSE)
 
-    expect_equal(
-        packageDescription("jlogger")$Package,
-        "jlogger"
-    )
+    expect_equal(packageDescription("jlogger")$Package,
+                "jlogger")
 })
 
-# test_that("github.packages dependencies work",
-# {
-#     # jsroot::dependencies(github.packages = list("wrathematics" = c("getip"),
-#     #                                             "RBigData" = c("remoter", "pbdCS")),
-#     #                      libpath = 'jsroot_lib_test')
-# })
+test_that("github.packages dependencies work",
+{
+    clean_before()
+
+    jsroot::dependencies(github.packages = list("tidyverse" = c("purrr")),
+                         libpath = 'jsroot_lib_test')
+
+    expect_equal(packageDescription("purrr")$Package, "purrr")
+})
